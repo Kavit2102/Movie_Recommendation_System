0f1fe6d3ba5f1edba44b87d1a2c7c0d73d086882
Kavit2102 <kavitdesai212002@gmail.com>
2026-07-21 19:52:30 +0530

    fix: update project name in package.json and format tsconfig.node.json

--- ui-showcase/src/components/movie/moviecard.tsx
+++ ui-showcase/src/components/movie/moviecard.tsx
@@ -1,8 +1,6 @@
-import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import {
     Card,
-    CardAction,
     CardDescription,
     CardFooter,
     CardHeader,
